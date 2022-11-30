5d9a9ebeba389650ee673c00b6e95d94b0463728
SzilviaFici <sziszfici@gmail.com>
2022-11-30 22:21:11 +0100

    commit3

---
+++ scripts/scriptEinungstest.js
@@ -0,0 +1,17 @@
+function TestErstellen (Fragen, FragenContainer, ErgebnisContainer, AbschickenButton){
+
+    function FragenAnzeigen (Fragen, FragenContainer){
+
+    }
+
+    function ErgebnisseAnzeigen (Fragen, FragenContainer, ErgebnisContainer){
+
+    }
+
+    FragenAnzeigen (Fragen, FragenContainerContainer);
+
+
+    anzeigeButton.onclick = function(){
+        ErgebnisseAnzeigen (Fragen, FragenContainer, ErgebnisContainer);
+    }
+}
